a0f9bbdb4cd3151257b02895018ffcab9f9aee88
Joshua David Dowling <n23jdd@mun.ca>
2014-11-22 18:26:58 -0330

    cxz

--- CS3715-PROJ/src/requestHandlers.js
+++ CS3715-PROJ/src/requestHandlers.js
@@ -1,55 +1,15 @@
 var querystring = require("querystring"),
-fs = require("fs"),
-formidable = require("formidable");
+fs = require("fs");
 function start(response) {
-console.log("Request handler 'start' was called.");
-var body = fs.readFile("./html/index.html", function(error, data){
-	if(error){
-		console.log(data);
-		response.writeHead(500, {"Content-Type":"text/html"});
-		response.write(data);
-		response.end();
-	}
-});
-response.writeHead(200, {"Content-Type": "text/html"});
-response.write(body);
-response.end();
-}
-function upload(response, request) {
-console.log("Request handler 'upload' was called.");
-var form = new formidable.IncomingForm();
-console.log("about to parse");
-form.parse(request, function(error, fields, files) {
-	console.log("parsing done");
-	/* Possible error on Windows systems:
-	tried to rename to an already existing file */
-	fs.rename(files.upload.path, "./tmp/test.png", function(error) {
-	if (error) {
-	fs.unlink("./tmp/test.png");
-	fs.rename(files.upload.path, "./tmp/test.png");
-	}
+	console.log("Request handler 'start' was called.");
+	var body = fs.readFile("./html/index.html", function(error){
+		if(error){
+			throw error;
+		}
 	});
+
 	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("received image:<br/>");
-	response.write("<img src='/show' />");
+	response.write(body);
 	response.end();
-	});
-	}
-	function show(response) {
-	console.log("Request handler 'show' was called.");
-	fs.readFile("./tmp/test.png", "binary", function(error, file) {
-	if(error) {
-	response.writeHead(500, {"Content-Type": "text/plain"});
-	response.write(error + "\n");
-	response.end();
-	} else {
-	response.writeHead(200, {"Content-Type": "image/png"});
-	response.write(file, "binary");
-	response.end();
-	}
-	});
-	}
+}
 	exports.start = start;
-	exports.upload = upload;
-	exports.show = show;
-	
