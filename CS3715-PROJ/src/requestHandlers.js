9004277b1f61d55d24e084728ece8f7e202ba7f6
Joshua David Dowling <n23jdd@mun.ca>
2014-11-30 16:01:04 -0330

    fsdafasd

--- CS3715-PROJ/src/requestHandlers.js
+++ CS3715-PROJ/src/requestHandlers.js
@@ -2,6 +2,7 @@ var querystring = require("querystring");
 var fs = require("fs");
 var url = require("url");
 var commentArray = new Array();
+var postArray = new Array();
 function GET(response, path){
     console.log("Data submitted by the user name:  " + path.query.name);
 
@@ -34,8 +35,15 @@ function event(response, request){
 		if(Object.keys(url_parts.query).length == 2){
 			var now = new Date();
 			console.log("Data submitted: "+url_parts.query.comments+" "+url_parts.query.author);
+
 			var newPost = [url_parts.query.author,url_parts.query.comments,now];
-		    commentArray.push(newPost);
+			if(url_parts.query.author == ("blogger")){
+				postArray.push(newPost);
+			}
+			else if(url_parts.query.author == ("visitor")){
+				commentArray.push(newPost);
+			}
+		    
 			saveComments();
 				
 		}
@@ -51,7 +59,7 @@ function saveComments(){
 	});
 	for(var i = 0; i < commentArray.length; i++){
 		fs.appendFile('./html/event/data/comments.txt',
-					"<li class = 'bloggerComments'>"+
+					"<li class = 'comment'>"+
 					commentArray[i][2]+"<br>"+
 					commentArray[i][1]+
 					"</li>"
@@ -64,6 +72,29 @@ function saveComments(){
 			throw error;
 		}
 	});
+
+	
+	fs.writeFile('./html/event/data/posts.txt',"<ul>", function(err){
+		if(err) throw err;
+	});
+	for(var i = 0; i < postArray.length; i++){
+		fs.appendFile('./html/event/data/posts.txt',
+					"<li class = 'post'>"+
+					postArray[i][2]+"<br>"+
+					postArray[i][1]+
+					"</li>"
+				, function(err){
+			if(err) throw err;
+		});
+	}
+	fs.appendFile('./html/event/data/posts.txt', "</ul>", function(error){
+		if(error){
+			throw error;
+		}
+	});
+	
+	
+	
 	console.log("Done!");
 }
 function loadComments(res){
@@ -72,9 +103,9 @@ function loadComments(res){
 			throw error;
 		}
 		
-		res.writeHead(200,{"Content-Type":"text/plain"});
-		console.log(commentString.toString());
+		res.writeHead(200,{"Content-Type":"text/html"});
 		res.write(commentString.toString());
+		
 		res.end();
 	});
 }
